81de9a28602c4e2f5713b4877091d23d94f60c7c
Raj-Shukla2002 <90229851+Raj-Shukla2002@users.noreply.github.com>
2022-04-04 21:11:00 -0400

    Delete csci2020-w22-assignment2 directory

--- csci2020-w22-assignment2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.csci2020w22assignment2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.csci2020w22assignment2 to javafx.fxml;
-    exports com.example.csci2020w22assignment2;
-}
